ef7e5f709a51a5a0aa854b0f0365f975d93d95ef
nachoz12341 <40530732+nachoz12341@users.noreply.github.com>
2019-03-11 15:25:55 -0500

    added layout
    changing computers need to push small additions

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main
+{
+    void HashLinear()
+    {
+    }
+
+    void HashQuadratic()
+    {
+    }
+
+    void HashDouble()
+    {
+    }
+
+    public static void main(String args[])
+    {
+
+    }
+}
